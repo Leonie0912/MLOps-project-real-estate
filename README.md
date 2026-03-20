9249fa94d725a809eeaeb0d69c050ab6752d92bc
Léonie <leoniewillecomme@orange.fr>
2026-03-20 09:33:50 +0100

    new structure

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # MLOps-project-real-estate
 
-Welcome to my final project for the MLOps class (DSBA M2). 
+March 9th, 2026 : 
 
-In this project, I build a tool for people who want to buy a property (a little bit like SeLoger). I also add a fraud detection feature and a "price is currently above / below average" feature. 
+Welcome to Léonie and Tommaso's final project for the MLOps class (DSBA M2). 
+
+In this project, we build a tool for people who want to buy a property (a little bit like SeLoger). We also add a fraud detection feature and a "price is currently above / below average" feature. 
+
+March 19th, 2026 : 
+
+This project started our with this dataset : Demande de Valeurs Foncières (DVF) from data.gouv.fr website. This dataset is the record of all real estate transactions that happened in France in 2024. 
+It is huge and messy, so it's important that we start this by cleaning the data. 
+
+The first step is adding a .gitignore : "*.txt". 
---
+++ src/data_cleaning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def clean_dvf(df: pd.DataFrame) -> pd.DataFrame:
+
+    #1. remove the columns we don't use or that are empty
+
+    unused = ['Identifiant de document', 'Reference document', '1 Articles CGI', '2 Articles CGI', '3 Articles CGI', '4 Articles CGI', '5 Articles CGI', 'Identifiant local']
+    df = df.drop(columns=unused, errors="ignore")
+
+    #2. keep only houses and apartments (no land, no farm, etc)
+
+    housing_types = ["Appartement", "Maison"]
+    df = df[df["Type local"].isin(housing_types)]
+
+    #3. we convert commas to points (for decimals), this is useful in "Valeur fonciere"
+
+    df["Valeur fonciere"] = (
+        df["Valeur fonciere"]
+        .astype(str)
+        .str.replace(',', '.')
+    )
+    df["Valeur fonciere"] = pd.to_numeric(df["Valeur fonciere"], errors="coerce")
+    
+    #4. removing null
+
+    df = df.dropna(subset=["Surface reelle bati", "Valeur fonciere"])
+
+    return df.copy()
